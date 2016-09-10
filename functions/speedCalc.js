7915410bd973b9834d435b0e684494dacd949ed9
Isaac <carmanwatts@gmail.com>
2016-09-10 18:00:45 -0300

    streamlining

---
+++ functions/speedCalc.js
@@ -0,0 +1,13 @@
+//Calculate speed based on bicycle data
+
+var cadence = 90;
+
+var ring = 48;
+
+var cog = 14;
+
+var wheel = 26;
+
+var mph = ((cadence*60)*(((wheel*3.14)/12)/5280))*(ring/cog);
+
+console.log(Math.round(mph*100)/100 + " mph");
